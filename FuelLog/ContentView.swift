ee9cca1f3784aaf781ddba0621b5aa70c425449d
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-06-12 12:00:51 +0800

    Initial Commit

---
+++ FuelLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FuelLog
+//
+//  Created by Muhammad Akbar Reishandy on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FuelLog/FuelLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelLogApp.swift
+//  FuelLog
+//
+//  Created by Muhammad Akbar Reishandy on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FuelLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
